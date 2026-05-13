1a3ff10353f2427a6ed7a6b7dc9ba027a3a24042
AlexMartG <amartinezg2@hotmail.com>
2026-05-13 12:35:34 +0200

    Level1 exercise2 completed

---
+++ level1/exercise2.php
@@ -0,0 +1,12 @@
+<?php
+
+$text = "Hello, World!";
+echo strtoupper($text) . "<br>";
+echo strlen($text) . "<br>";
+echo strrev($text) . "<br>";
+
+$newText = "Aquest és el curs de PHP";
+
+echo $text . " " . $newText;
+
+?>
