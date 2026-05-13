59463ea7a4767e01cfb98671c0ecbca2bd85daaf
AlexMartG <amartinezg2@hotmail.com>
2026-05-13 12:06:45 +0200

    Level1 exercise1 completed

--- level1/exercise1.php
+++ level1/exercise1.php
@@ -1,3 +1,17 @@
 <?php
 
+$age = 20;
+$height = 1.75;
+$city = "New York";
+$smoker = true;
+
+echo "Integer: " . $age . "<br>";
+echo "Double: " . $height . "<br>";
+echo "String: " . $city . "<br>";
+echo "Boolean: " . ($smoker ? "Yes" : "No") . "<br>";
+
+define("NAME", "Alex");
+
+echo ucwords(NAME) . "<br>";
+
 ?>
